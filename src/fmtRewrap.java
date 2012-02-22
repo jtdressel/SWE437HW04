60a4c7b1406d91298d300278034e1431a927938f
James Dressel <ubergeek@jamesdressel.com>
2012-02-22 14:18:25 -0500

    First commit. Code from assignment 03

---
+++ src/fmtRewrap.java
@@ -0,0 +1,43 @@
+//fmtRewrap.java
+//James Dressel
+public class fmtRewrap {
+	static public String fmtRewrap(String input, int width){
+		if (input== null){
+			throw new NullPointerException("The input string must not be null");
+		}
+		
+		if (width <1){
+			throw new IllegalArgumentException("Width must be at least 1");
+		}
+		StringBuilder build = new StringBuilder();
+		StringBuilder line = new StringBuilder();
+		String[] words = input.split(" ");
+		
+
+		for(String word:words){
+			int projectedLength = line.length()+word.length()+1;
+			
+			if(line.length()==0){
+				//nothing in the line
+				line.append(word);
+			} else if(projectedLength<width){
+				line.append(" ");
+				line.append(word);
+			} else{
+				build.append(line.toString());
+				build.append("\n");
+				line = new StringBuilder(word);
+				
+			}
+			
+			
+		} 
+		
+		build.append(line.toString());
+		
+		
+		
+		build.append("\n");
+		return build.toString();
+	}
+}
---
+++ src/fmtRewrapTest.java
@@ -0,0 +1,78 @@
+//fmtRewrapTest.java
+//James Dressel
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)//Best practices would break this file into
+		//multiple files, but I figured ease of grading trumps performance
+public class fmtRewrapTest {
+	
+	String inputString;
+	int width;
+	String expected;
+
+	
+	public fmtRewrapTest(String inputString, int width, String expected){
+		this.inputString = inputString;
+		this.width = width;
+		this.expected = expected;
+	}
+	
+	@Parameterized.Parameters 
+	public static Collection primeNumbers(){
+		return Arrays.asList(new Object[][]{
+				{"fox", 30, "fox\n"},
+				{"box", 30, "box\n"}	
+		});
+	}
+	
+	@Test
+	public void testSingleWord() {
+		assertEquals(expected, fmtRewrap.fmtRewrap(inputString, width));
+	}
+		
+	@Test
+	public void testFmtRewrapLongSentence(){
+		inputString = "This sentence is longer than the width so it should take multiple lines";
+		width = 30;
+		expected = "This sentence is longer than\nthe width so it should take\nmultiple lines\n";
+		assertEquals(expected, fmtRewrap.fmtRewrap(inputString, width));
+	}
+	
+	
+	@Test
+	public void testFmtRewrapWordLongerThanWidth(){
+		inputString = "This sentence is longer than the width so it should take multiple lines";
+		width = 5;
+		expected = "This\nsentence\nis\nlonger\nthan\nthe\nwidth\nso\nit\nshould\ntake\nmultiple\nlines\n";
+		assertEquals(expected, fmtRewrap.fmtRewrap(inputString, width));
+	}
+	
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testFmtRewrapZeroWidth(){
+		inputString = "Fox";
+		width = 0;
+		fmtRewrap.fmtRewrap(inputString, width);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testFmtRewrapNegativeWidth(){
+		inputString = "Fox";
+		width = -5;
+		fmtRewrap.fmtRewrap(inputString, width);
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void testFmtRewrapNullInput(){
+		width = 5;
+		fmtRewrap.fmtRewrap(null, width);
+	}
+
+}
